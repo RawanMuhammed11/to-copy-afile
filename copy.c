038811ccfd644d5315d81918393ffdda5d972e48
rawannn <RawanMuhammed11@github.com>
2022-08-12 15:38:20 +0200

    to copy content of any file to another

--- copy.c
+++ copy.c
@@ -6,30 +6,42 @@
 
 #define BSIZE 16000
 
-int main()
+int main(int argc , char *argv[])
 {
 int fin,fout;
 char buf[BSIZE];
 int count;
 
-if( (fin=open("foo",O_RDONLY)) <0)
- { 
-   perror("there is an error in foo file");
-   exit(1);
- }
-if( (fout=open("boo",O_WRONLY|O_CREAT,0644))<0 )
- {
-   perror("there is an error in boo file"); 
-   exit(2); 
- }
+
+if(argc<3)
+{
+  printf("you must add 3 argument -> copy + srcfile + dstfile\n" );
+
+}
+else
+{
+
+
+  if(( (fin=open(argv[1],O_RDONLY)) <0))
+     
+   printf("there is an error in this file : %s\n ",argv[1]);
+  
+ else
+   {
+  if( ((fout=open(argv[2],O_WRONLY|O_CREAT,0644))<0) )
+     
+   printf("there is an error in this file: %s\n ",argv[2]); 
+ 
+   }
+
 //to copy content of foo file into boo
 
 while( (count=read(fin,buf,BSIZE))>0 )
  { 
-   write(fout,buf,count);   
+ write(fout,buf,count);   
  }
 close(fin);
 close(fout);
 
- 
+} 
 }
